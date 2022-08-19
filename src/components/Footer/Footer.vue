29b61eb15f880f84fac3e4ab5aa21c529834962a
jsdevraju <islamrajulearn.dev@gmail.com>
2022-08-19 19:45:41 +0600

    added footer option

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,15 +1,69 @@
 <template>
-    <div>
-            <h1>Footer</h1>
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10">
+          <div class="footer__content">
+            <div class="footer__content__logo">
+              <img src="../../assets/logo.png" alt="logo" width="150" />
+            </div>
+            <div class="footer__content__text">
+              <p>
+                Cointy je rogetorana anata spaleenot Trinity Capital LC Tato
+                spole@os oencoaná orgánem Financial Servioes Audhorty of s
+                Vincent & The Grenadiness 6biom porolon 450 L1C 2020
+              </p>
+              <p>
+                Omeznd jurisdkcec Nezakdime üity obyvatelüm urCityah juatiodkci
+                (vec USAL P ice anfonmaciai phečtito vaobecns cbchadni podminky
+              </p>
+            </div>
+            <div class="d-flex footer__content__social">
+              <a>VSeobeond olohodnl podmik</a>
+              <p>ohrana 0sobnich üdaj</p>
+            </div>
+            <!-- Copy Right text -->
+            <div>
+                <p>Copyright 2021 2022 Colsy All nghts resarvwd.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <h3>Sociální odkazy</h3>
+          
+        </div>
+      </div>
     </div>
+  </footer>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
-<style>
+<style scoped>
+.footer {
+  background-color: #f3f3f3;
+  padding: 5em 0px;
+}
 
+.footer__content__text{
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 1.5em;
+}
+
+.footer__content__social{
+    gap: 10px;
+}
+.footer__content__social a{
+    text-decoration: underline !important;
+    cursor: pointer;
+}
+.footer__content__text p:first-child{
+    width: 50%;
+}
+.footer__content__text p:last-child{
+    width: 50%;
+}
 </style>
--- src/sections/About/About.vue
+++ src/sections/About/About.vue
@@ -15,16 +15,75 @@
         <p class="text-white">Proto jsme spojili sily s Nathanem.</p>
       </div>
       <!-- About With Image -->
-      <div class="row gap">
-        <div class="col-md-6">
+      <div class="row">
+        <div class="col-md-3">
           <img src="../../assets/about.png" alt="Razu Islam" />
         </div>
-        <div class="col-md-6">
-          <p>
-            Přinášíme Vám nejen možnost podilet se s námi na Nathanově rüstu,
-            ale i možnost vytvářet si
+        <div class="col-md-9">
+          <p class="text-color">
+            Přinášíme Vám nejen možnost podilet se s námi na Nathanově <br />
+            rüstu, ale i možnost vytvářet si
           </p>
-          <h1>BUDOUCNOST V KRYPTOSVĚTĚ</h1>
+          <h1 class="title">BUDOUCNOST V KRYPTOSVĚTĚ</h1>
+        </div>
+      </div>
+      <!-- Play Store -->
+      <div class="row">
+        <div class="col-md-8">
+          <Title title="Jsme Coinfy" />
+          <p class="text-color">
+            S námi müžete obchodovat kryptoměny jednoduše a odkudkoli,
+            <br />
+            diky platformě DXtrade.
+          </p>
+          <p class="text-color">
+            Müžete obchodovat na nárüst trhu, ale i vydělávat, pokud trhy
+            klesaji.
+          </p>
+          <div class="store_img d-flex">
+            <img src="../../assets/app.png" alt="Client" />
+            <img src="../../assets/play.png" alt="Client" />
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <img src="../../assets/mobile.png" alt="Client Images" />
+        </div>
+      </div>
+      <!-- Card  -->
+      <div class="row">
+        <div class="col-md-6">
+          <div class="d-flex align-items-center cardBg rounded">
+            <div class="bg-content">
+              <h4 class="text-white">Jen připomeneme</h4>
+              <p class="text-white">
+                Nathan Dzalba je mediäli hvězda, profesionálni fotbalista,
+                influencer a MMA zápasnik, kterého müžete znát z řady TV reality
+                show -Survivor, Kdo přežije, Love Island nebo Svatba na prvni
+                pohled. Nathana jste mohli vidět napřiklad také v pěvecké
+                soutěži SuperStar.
+              </p>
+            </div>
+            <div class="position-absolute client_img">
+              <img src="../../assets/client.png" alt="Client Image" />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 bgImg rounded">
+          <div class="card-content">
+            <p class="text-white">
+              Pokud se rozhodnete začit, <br />
+              diky Nathanovi obdržite
+            </p>
+
+            <h1 class="text-white">
+              bonus 20 % <br />
+              7 na prvni vklad
+            </h1>
+            <button class="btn app_btn text-white">
+              Chci se zaregistrovat
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -32,7 +91,8 @@
 </template>
 
 <script>
-export default {};
+import Title from "../../components/Title/Title.vue";
+export default { components: { Title } };
 </script>
 
 <style scoped>
@@ -57,4 +117,56 @@ export default {};
   margin-top: 1.2em;
 }
 
+.title {
+  color: var(--primary-color);
+  font-size: 1.5em;
+}
+
+.store_img {
+  margin-top: 3em;
+  gap: 10px;
+}
+
+.store_img img {
+  width: 120px !important;
+}
+
+.row {
+  margin-top: 5em;
+}
+.row img {
+  width: 300px;
+}
+
+.cardBg {
+  background-color: var(--primary-color);
+  position: relative;
+}
+
+.bg-content,
+.card-content {
+  padding: 2em;
+}
+
+.bg-content p {
+  max-width: 250px;
+}
+
+.client_img {
+  top: -93px;
+  right: 10px;
+}
+
+.bgImg {
+  background-image: linear-gradient(#22222285, #22222293),
+    url("../../assets/bg.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.app_btn {
+  background-color: var(--primary-color);
+  margin-top: 1em;
+}
 </style>
